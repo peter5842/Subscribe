be334ac80179028b3cbeb3b5f610050d5867edbe
liudq <595207742@qq.com>
2022-12-16 00:23:16 +0800

    update_sysu

--- js/sysu.js
+++ js/sysu.js
@@ -1,4 +1,4 @@
-var ssidMatched = /SYSU-SECURE*/.test($network.wifi.ssid);
+var ssidMatched = /SYSU-SECURE.*/.test($network.wifi.ssid);
 if (ssidMatched) {
 	$done({servers: new Array("10.8.8.8","10.8.4.4")});
 } else {
